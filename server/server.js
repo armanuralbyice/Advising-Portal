3b02867277220d95dcc02bfeb79a24ed873287c6
armanuralbyice <armanurhossainalbyice0@gmail.com>
2024-06-30 21:19:34 +0600

    some changes for deploy the project

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://advising-portal-two.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Authorization', 'Content-Type']
