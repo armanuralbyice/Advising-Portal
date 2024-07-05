44b3c93433b9dfb78a5d4e0543a6fb96b8c578ed
armanuralbyice <armanurhossainalbyice0@gmail.com>
2024-07-05 14:52:34 +0600

    for deploy

--- server1/router/semester.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createSemester, getAllSemester, deleteSemester } = require("../controller/SemesterController");
-
-const router = express.Router();
-router.route('/create').post(createSemester)
-router.route('/all').get(getAllSemester)
-router.route('/delete/:id').delete(deleteSemester)
-
-module.exports = router;
