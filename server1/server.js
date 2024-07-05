c660f24712aa0cf3b62ca5faa41824cb1996b4dc
armanuralbyice <armanurhossainalbyice0@gmail.com>
2024-07-05 14:45:03 +0600

    for deploy

--- server1/server.js
+++ server1/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: "https://localhost:3000",
+        origin: "https://advising-portal-l5lx.vercel.app",
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         allowedHeaders: ["Authorization", "Content-Type"],
